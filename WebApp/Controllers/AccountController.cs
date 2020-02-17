3d1d3d555e8843abe8a189c3599b52ff921583da
artem <david.williams322solo@gmail.com>
2020-02-17 23:31:37 +0300

    started creating account controller

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("account/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("account/login")]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            return View(loginModel);
+        }
+        [Route("account/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
